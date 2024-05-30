1f57b03ae5b1b5665339a0767259437579e41626
TwiceInEveryMoment <elamderek574@gmail.com>
2024-05-30 10:51:26 -0400

    Fix incorrect spell-learned message in browser

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -1815,16 +1815,15 @@ export class NewEraActorSheet extends ActorSheet {
             this.actor.actionMessage(this.actor.img, spellFromCompendium.img, "{NAME} learned {0}!", spellFromCompendium.name);
             if (dropData.remaining > 1){
               ui.notifications.info(`You learned ${spellFromCompendium.name}! You can choose ${dropData.remaining-1} more spell${dropData.remaining>2?'s':''} from the current study guide.`);
-            } else if (dropData.remaining == -1){
-              ui.notifications.info(`You learned ${spellFromCompendium.name}!`);
-            } else {
+            } else if (dropData.remaining === 0){
               ui.notifications.info(`You learned ${spellFromCompendium.name}! You've completed this study guide.`);
+            } else if (dropData.remaining == -1 || dropData.remaining === null){
+              ui.notifications.info(`You learned ${spellFromCompendium.name}!`);
             }
+          }
         } else {
           ui.notifications.error(`Error: Couldn't load spell data`);
         }
       }
     }
-  }
-
 }
