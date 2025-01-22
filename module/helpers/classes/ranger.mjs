d00bc0f85b4b32559827cafcecd1836ec9d068c2
twiceineverymoment <emilyjelam27@gmail.com>
2025-01-22 15:14:43 -0500

    Ranger Deadeye table change

--- module/helpers/classes/ranger.mjs
+++ module/helpers/classes/ranger.mjs
@@ -237,7 +237,7 @@ export class Ranger {
                     field: "deadeyeDamage",
                     label: "Damage per Frame",
                     sign: true,
-                    values: [null, 0, 0, 0, 0, 0, 0, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5]
+                    values: [null, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4]
                 }
             ],
             actions: [
